16967c769d0cd9f70f367804ea4c84a5ed5b5c72
ghakg07 <ghakg07@student.lnu.se>
2013-11-11 16:25:19 +0100

    Första testet
    Lite bugg med testprogram. (Tvungen att gå in i test-settings och välja
    x64 och cleana. Annars gick det bra att testa utifrån testtutorialn.

---
+++ UnitTestTri/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTri
+{
+    [TestClass]
+    public class TriangleUnitTest1
+    {
+        [TestMethod]
+        public void isIsoscelesTest()
+        {
+            Triangle tri = new Triangle(1.0, 0.5, 1.0);
+            Assert.IsTrue(tri.isIsosceles());
+        }
+    }
+}
